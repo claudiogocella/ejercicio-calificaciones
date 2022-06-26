86f8263281eb5aa728970fcf6a40b54749273568
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-06-26 00:48:10 -0300

    correcion en lineas de codigo modifique lieas 8 a 13

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-let nombreAlumno = prompt ( "Ingrese el nombre del alumno (vacío para cortar)" ) ; 
-let notaPractica , notaProblemas , notaTeorica , notaFinal : numero ;
-while ( nombreAlumno !== "" ) {
-notaPractica = Numero ( prompt ( "Nota práctica: " ) ) ;
-notaProblemas = Numero ( prompt ( "Nota problemas: " ) ) ;
-notaTeorica = Numero ( prompt ( "Nota teórica: " ) ) ;
-if ( ( notaPractica <= 10 && notaPractica >= 0 ) && ( notaProblemas <= 10 && notaProblemas >= 0 ) && 
-( notaTeorica <= 10 && notaTeorica >= 0 ) {
-notaFinal = notaPractica * 0.1 + notaProblemas * 0.5 + notaTeorica * 0.4 ;
-consola _ log ( "La nota final de" , nombreAlumno , "es:" , notaFinal ) ;
-} mas {
-consola _ log ( "Error en las notas ingresadas" ) ;
-}
-nombreAlumno = prompt ( "Ingrese el nombre del alumno (vacío para cortar)" ) ;
+let nombreAlumno = prompt("Ingrese el nombre del alumno (vacío para cortar)");
+let notaPractica, notaProblemas, notaTeorica, notaFinal: number;
+while (nombreAlumno !== "") {
+  notaPractica = Number(prompt("Nota práctica: "));
+  notaProblemas = Number(prompt("Nota problemas: "));
+  notaTeorica = Number(prompt("Nota teórica: "));
+  if (
+    notaPractica <= 10 &&
+    notaPractica >= 0 &&
+    notaProblemas <= 10 &&
+    notaProblemas >= 0 &&
+    notaTeorica <= 10 &&
+    notaTeorica >= 0
+  ) {
+    notaFinal = notaPractica * 0.1 + notaProblemas * 0.5 + notaTeorica * 0.4;
+    console.log("La nota final de ", nombreAlumno, " es: ", notaFinal);
+  } else {
+    console.log("Error en las notas ingresadas");
+  }
+  nombreAlumno = prompt("Ingrese el nombre del alumno (vacío para cortar)");
 }
